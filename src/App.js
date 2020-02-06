36a040d25c9f7e89b98a2a957064e8e63558c4fd
Haram Bae <hisfedev@gmail.com>
2020-02-06 17:54:20 +0900

    :sparkles: Add react-router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import GlobalStyle from 'styles/global';
-import LoginPopup from 'components/LoginPopup/LoginPopup';
+import SignUp from 'pages/SignUp/SignUp';
 import Home from './pages/Home/Home';
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <Home />
-      <LoginPopup />
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/signup">
+            <SignUp />
+          </Route>
+        </Switch>
+      </Router>
     </>
   );
 }

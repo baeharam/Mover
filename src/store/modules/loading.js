d4784bdb61b59829c7d85609946f698fb2a0d9ea
Haram Bae <hisfedev@gmail.com>
2020-02-10 15:26:35 +0900

    :heavy_minus_sign: Remove flow

--- src/store/modules/loading.js
+++ src/store/modules/loading.js
@@ -1,29 +1,15 @@
-
-import { createAction, handleActions, type ActionType } from 'redux-actions';
+import { createAction, handleActions } from 'redux-actions';
 import produce from 'immer';
 
 export const START_LOADING = 'START_LOADING';
 export const FINISH_LOADING = 'FINISH_LOADING';
 
-export const startLoading = createAction<string, {| type: string |}>(
-  START_LOADING,
-);
-export const finishLoading = createAction<string, {| type: string |}>(
-  FINISH_LOADING,
-);
-
-type StartLoadingActionType = ActionType<typeof startLoading>;
-type FinishLoadingActionType = ActionType<typeof finishLoading>;
-
-type LoadingActionType = StartLoadingActionType | FinishLoadingActionType;
-
-type LoadingStateType = {
-  [key: string]: boolean,
-};
+export const startLoading = createAction(START_LOADING);
+export const finishLoading = createAction(FINISH_LOADING);
 
 const initialState = {};
 
-const reducer = handleActions<LoadingStateType, LoadingActionType>(
+const reducer = handleActions(
   {
     [START_LOADING]: (state, action) => {
       return produce(state, draft => {

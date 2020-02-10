54e4dbe5f5ad26bd4e7952f640b32e6efe7cfb02
Haram Bae <hisfedev@gmail.com>
2020-02-10 15:05:08 +0900

    :sparkles: Add signup saga

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -2,7 +2,8 @@
 
 import { all, call } from 'redux-saga/effects';
 import login from './login';
+import signup from './signup';
 
 export default function* rootSaga(): Generator<any, void, any> {
-  yield all([call(login)]);
+  yield all([call(login), call(signup)]);
 }

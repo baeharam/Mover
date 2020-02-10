8aa4d8a16c9b58339eeec831deb939c77d48a30f
Haram Bae <hisfedev@gmail.com>
2020-02-10 15:04:39 +0900

    :sparkles: Add loader

--- src/store/modules/index.js
+++ src/store/modules/index.js
@@ -4,9 +4,11 @@ import { combineReducers } from 'redux';
 import login from './login';
 import signup from './signup';
 import user from './user';
+import loading from './loading';
 
 export default combineReducers({
   login,
   signup,
   user,
+  loading,
 });

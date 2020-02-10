552a9db6bb72d171cfe5b5901df51dcae79f246e
Haram Bae <hisfedev@gmail.com>
2020-02-10 16:09:44 +0900

    :bug: Remove path name from screen

--- src/utils/router.js
+++ src/utils/router.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { withRouter } from 'react-router';
 
 const LocationDisplay = withRouter(({ location }) => (
-  <div data-testid="location-display">{location.pathname}</div>
+  <div data-testid="location-display" style={{ textIndent: '-9999px' }}>
+    {location.pathname}
+  </div>
 ));
 
 export default LocationDisplay;
